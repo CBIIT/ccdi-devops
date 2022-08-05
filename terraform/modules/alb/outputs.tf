c02d6c544da1f357397e119a6430dc8b1eeaa5e6
Cole DeVries <colemandevries@gmail.com>
2022-08-05 06:24:44 -0400

    Updated the ALB module outputs #patch

---
+++ terraform/modules/alb/outputs.tf
@@ -0,0 +1,16 @@
+output "id" {
+  value = aws_lb.alb.id
+}
+
+output "arn" {
+  value = aws_lb.alb.arn
+}
+
+output "arn_suffix" {
+  value = aws_lb.alb.arn_suffix
+}
+
+output "dns_name" {
+  value = aws_lb.alb.dns_name
+}
+
