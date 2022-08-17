03972396d9a980ce1bd5c68583260e96d8198377
colemandevries <colemandevries@gmail.com>
2022-08-17 10:27:50 -0400

    updated the outputs for the alb construct #patch

---
+++ terraform/constructs/alb/outputs.tf
@@ -0,0 +1,15 @@
+output "alb_id" {
+  value = module.alb.id
+}
+
+output "alb_arn" {
+  value = module.alb_arn
+}
+
+output "security_group_id" {
+  value = module.security_group.id
+}
+
+output "security_group_arn" {
+  value = module.security_group.arn
+}
