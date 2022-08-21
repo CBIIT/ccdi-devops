0512bd0f1e583819da73a61ab2ac0660582d3da5
Cole DeVries <colemandevries@gmail.com>
2022-08-21 17:24:51 -0400

    added prevent_destroy to opensearch policy module #patch

--- terraform/modules/opensearch/main.tf
+++ terraform/modules/opensearch/main.tf
@@ -3,6 +3,10 @@ resource "aws_iam_service_linked_role" "os" {
   
   aws_service_name = "opensearchservice.amazonaws.com"
   description      = "creates the AWSServiceRoleForAmazonOpenSearchService role"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_opensearch_domain" "os" {
