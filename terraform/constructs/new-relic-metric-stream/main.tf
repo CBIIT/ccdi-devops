f85cc9fc934d796b1df0dbe59029875c8cedd172
Cole DeVries <colemandevries@gmail.com>
2022-08-28 11:53:01 -0400

    finishing the new-relic-metric-stream construct #patch

--- terraform/constructs/new-relic-metric-stream/main.tf
+++ terraform/constructs/new-relic-metric-stream/main.tf
@@ -19,5 +19,23 @@ module "cloudwatch_metric_stream" {
 module "kinesis_firehose" {
   source = "../../modules/kinesis-firehose/"
 
+  #Required Variables
+  account_id               = var.account_id
+  app                      = var.app
+  external_id              = var.external_id
+  http_endpoint_access_key = var.http_endpoint_access_key
+  level                    = var.level
+  program                  = var.program
 
+  #Optional Variables 
+  buffer_interval        = var.buffer_interval
+  buffer_size            = var.buffer_size
+  content_encoding       = var.content_encoding
+  destination            = var.destination
+  http_endpoint_name     = var.http_endpoint_name
+  http_endpoint_url      = var.http_endpoint_url
+  s3_backup_mode         = var.s3_backup_mode
+  s3_compression_format  = var.s3_compression_format
+  s3_error_output_prefix = var.s3_error_output_prefix
+  s3_object_prefix       = var.s3_object_prefix
 }
