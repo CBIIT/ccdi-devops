ed0b0f62737b8e60b4c7ca74fb13d54dda89531b
Sowmya Karavadi <ksowmya442@gmail.com>
2022-09-02 17:29:18 -0400

    updated secret_manager_name

--- terraform/modules/secrets-manager/main.tf
+++ terraform/modules/secrets-manager/main.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "secret"{
-  name         = "${var.program}_${var.app}_${terraform.workspace}_creds"
+  name         = local.secret_manager_name
   description  = "Credentials used for monitoring and database services"
 }
 
