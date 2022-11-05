14dc338d9c115e62a19b4f723f152e103ea6f935
Cole DeVries <colemandevries@gmail.com>
2022-11-05 12:28:01 -0400

    broke down workload module by tier

--- terraform/newrelic/modules/workload/main.tf
+++ terraform/newrelic/modules/workload/main.tf
@@ -1,8 +1,36 @@
-resource "newrelic_workload" "this" {
+resource "newrelic_workload" "prod" {
   account_id = var.account_id
-  name       = local.stack
+  name       = "${var.program}-prod-${var.app}"
 
   entity_search_query {
-    query = local.workload_query
+    query = local.query_prod
   }
 }
+
+resource "newrelic_workload" "stage" {
+  account_id = var.account_id
+  name       = "${var.program}-stage-${var.app}"
+
+  entity_search_query {
+    query = local.query_stage
+  }
+}
+
+resource "newrelic_workload" "qa" {
+  account_id = var.account_id
+  name       = "${var.program}-qa-${var.app}"
+
+  entity_search_query {
+    query = local.query_qa
+  }
+}
+
+resource "newrelic_workload" "dev" {
+  account_id = var.account_id
+  name       = "${var.program}-dev-${var.app}"
+
+  entity_search_query {
+    query = local.query_dev
+  }
+}
+
--- terraform/newrelic/modules/workload/outputs.tf
+++ terraform/newrelic/modules/workload/outputs.tf
@@ -1,15 +1,32 @@
-output "name" {
-  value = newrelic_workload.this.name
+output "prod_guid" {
+  value = newrelic_workload.prod.guid
 }
 
-output "guid" {
-  value = newrelic_workload.this.guid
+output "stage_guid" {
+  value = newrelic_workload.stage.guid
 }
 
-output "workload_id" {
-  value = newrelic_workload.this.workload_id
+output "qa_guid" {
+  value = newrelic_workload.qa.guid
 }
 
-output "permalink" {
-  value = newrelic_workload.this.permalink
+output "dev_guid" {
+  value = newrelic_workload.dev.guid
+}
+
+
+output "prod_workload_id" {
+  value = newrelic_workload.prod.workload_id
+}
+
+output "stage_workload_id" {
+  value = newrelic_workload.stage.workload_id
+}
+
+output "qa_workload_id" {
+  value = newrelic_workload.qa.workload_id
+}
+
+output "dev_workload_id" {
+  value = newrelic_workload.dev.workload_id
 }
