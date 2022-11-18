f388f5cb155367917248ca5ba7d81d351a32ad62
colemandevries <colemandevries@gmail.com>
2022-11-18 11:21:59 -0500

    updated ecs cluster resource name to follow conventions

--- terraform/modules/ecs-cluster/main.tf
+++ terraform/modules/ecs-cluster/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecs_cluster" "cluster" {
   # checkov:skip=CKV_AWS_65: Container insights are enabled by default
-  name = "${var.program}-${var.app}-${var.tier}-ecs-cluster"
+  name = "${var.program}-${var.tier}-${var.app}-ecs-cluster"
 
   setting {
     name  = "containerInsights"
