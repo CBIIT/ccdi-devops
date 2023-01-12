929f7beacf82bf0d16609e713bc126d25443fafd
kiran <kotepalliv2@nih.gov>
2023-01-11 19:43:28 -0500

    upated outputs for S3

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -12,3 +12,8 @@ output "bucket" {
   value       = aws_s3_bucket.s3.bucket
   description = "The S3 Bucket name (bucket)"
 }
+
+output "s3_bucket_domain_name" {
+  description = "The bucket domain name. Will be of format bucketname.s3.amazonaws.com."
+  value       = aws_s3_bucket.s3.bucket_domain_name
+}
