824706ad11b17a398f0fcf6d4bc24a1c74c86cb1
Cole DeVries <colemandevries@gmail.com>
2023-02-26 14:41:37 -0500

    trying a new format

--- terraform/aws/modules/subnets/main.tf
+++ terraform/aws/modules/subnets/main.tf
@@ -1,3 +1,15 @@
+##################################################################################################################################
+## VPC ###########################################################################################################################
+##################################################################################################################################
+
+data "aws_vpc" "vpc" {
+
+  filter {
+    name   = "tag:Name"
+    values = ["*${var.env}*"]
+  }
+}
+
 #############################################################
 ## Public Subnets ###########################################
 #############################################################
@@ -13,70 +25,70 @@ data "aws_subnets" "public" {
   }
 }
 
-data "aws_subnet" "public_1" {
-  id = element(sort(tolist(data.aws_subnets.public.ids)), 0)
+# data "aws_subnet" "public_1" {
+#   id = element(sort(tolist(data.aws_subnets.public.ids)), 0)
 
-  depends_on = [
-    data.aws_subnets.public
-  ]
-}
+#   depends_on = [
+#     data.aws_subnets.public
+#   ]
+# }
 
-data "aws_subnet" "public_2" {
-  id = element(sort(tolist(data.aws_subnets.public.ids)), 1)
+# data "aws_subnet" "public_2" {
+#   id = element(sort(tolist(data.aws_subnets.public.ids)), 1)
 
-  depends_on = [
-    data.aws_subnets.public
-  ]
-}
+#   depends_on = [
+#     data.aws_subnets.public
+#   ]
+# }
 
-#############################################################
-## WebApp Subnets ###########################################
-#############################################################
+# #############################################################
+# ## WebApp Subnets ###########################################
+# #############################################################
 
-data "aws_subnets" "webapp" {
+# data "aws_subnets" "webapp" {
 
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
+#   filter {
+#     name   = "vpc-id"
+#     values = [var.vpc_id]
+#   }
 
-  tags = {
-    Name = "sn-${var.env}-webapp-us-east-*"
-  }
-}
+#   tags = {
+#     Name = "sn-${var.env}-webapp-us-east-*"
+#   }
+# }
 
-data "aws_subnet" "webapp_1" {
-  id = element(sort(tolist(data.aws_subnets.webapp.ids)), 0)
+# data "aws_subnet" "webapp_1" {
+#   id = element(sort(tolist(data.aws_subnets.webapp.ids)), 0)
 
-  depends_on = [
-    data.aws_subnets.webapp
-  ]
-}
+#   depends_on = [
+#     data.aws_subnets.webapp
+#   ]
+# }
 
-data "aws_subnet" "webapp_2" {
-  id = element(sort(tolist(data.aws_subnets.webapp.ids)), 1)
-}
+# data "aws_subnet" "webapp_2" {
+#   id = element(sort(tolist(data.aws_subnets.webapp.ids)), 1)
+# }
 
-#############################################################
-## Database Subnets #########################################
-#############################################################
+# #############################################################
+# ## Database Subnets #########################################
+# #############################################################
 
-data "aws_subnets" "database" {
+# data "aws_subnets" "database" {
 
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
+#   filter {
+#     name   = "vpc-id"
+#     values = [var.vpc_id]
+#   }
 
-  tags = {
-    Name = "sn-${var.env}-db-us-east-*"
-  }
-}
+#   tags = {
+#     Name = "sn-${var.env}-db-us-east-*"
+#   }
+# }
 
-data "aws_subnet" "database_1" {
-  id = element(sort(tolist(data.aws_subnets.database.ids)), 0)
-}
+# data "aws_subnet" "database_1" {
+#   id = element(sort(tolist(data.aws_subnets.database.ids)), 0)
+# }
 
-data "aws_subnet" "database_2" {
-  id = element(sort(tolist(data.aws_subnets.database.ids)), 1)
-}
+# data "aws_subnet" "database_2" {
+#   id = element(sort(tolist(data.aws_subnets.database.ids)), 1)
+# }
