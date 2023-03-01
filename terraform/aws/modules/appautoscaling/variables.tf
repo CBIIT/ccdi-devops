7dccf506f16f13a9906bbb19affbaa1d5daf57fa
Cole DeVries <colemandevries@gmail.com>
2023-02-28 19:50:24 -0500

    added sensitivity declaration to appautoscaling variables

--- terraform/aws/modules/appautoscaling/variables.tf
+++ terraform/aws/modules/appautoscaling/variables.tf
@@ -1,5 +1,3 @@
-### policy 
-
 variable "app" {
   type        = string
   description = "the name of the application expressed as an acronym"
@@ -31,56 +29,66 @@ variable "program" {
 variable "ecs_cluster_name" {
   type        = string
   description = "name of the ecs cluster that the autoscaling target belongs to"
+  sensitive   = false
 }
 
 variable "ecs_service_name" {
   type        = string
   description = "name of the ecs service that the autoscaling target belongs to"
+  sensitive   = false
 }
 
 variable "max_capacity" {
   type        = number
   description = "max capacity of the scalable target"
   default     = 4
+  sensitive   = false
 }
 
 variable "microservice" {
   type        = string
   description = "the name of the microservice for the app autoscaling policy and target - i.e. frontend, backend, etc."
+  sensitive   = false
 }
 
 variable "min_capacity" {
   type        = number
   description = "minimum capacity of the scalable target"
   default     = 1
+  sensitive   = false
 }
 
 variable "policy_type" {
   type        = string
   description = "scaling policy - either 'StepScaling' or 'TargetTrackingScaling'"
   default     = "TargetTrackingScaling"
+  sensitive   = false
 }
 
 variable "scalable_dimension" {
   type        = string
   description = "calable dimension of the scalable target - use outputs from appautoscaling target resource"
   default     = "ecs:service:DesiredCount"
+  sensitive   = false
 }
 
 variable "service_namespace" {
   type        = string
   description = "aws service namespace of the scalable target - use outputs from the appautoscaling target resource"
   default     = "ecs"
+  sensitive   = false
 }
 
 variable "target_value" {
   type        = number
   description = "target value of the metric to invoke autoscaling activity"
   default     = 80
+  sensitive   = false
 }
 
 variable "predefined_metric_type" {
   type        = string
   description = "the name of the pre-defined aws cloudwatch metric type to base autoscaling activities on"
   default     = "ECSServiceAverageCPUUtilization"
+  sensitive   = false
 }
