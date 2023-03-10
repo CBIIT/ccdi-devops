2a1a30e011fc9bca627775ff52805d9642cf560b
colemandevries <colemandevries@gmail.com>
2023-03-10 13:32:14 -0500

    updated outputs for secrets manager module

--- terraform/aws/modules/secrets-manager/outputs.tf
+++ terraform/aws/modules/secrets-manager/outputs.tf
@@ -1,47 +1,47 @@
 output "arn" {
   value       = aws_secretsmanager_secret.this.arn
-  description = ""
+  description = "arn of the secret"
   sensitive   = false
 }
 
 output "id" {
   value       = aws_secretsmanager_secret.this.id
-  description = ""
+  description = "id of the secret"
   sensitive   = false
 }
 
 output "name" {
   value       = aws_secretsmanager_secret.this.name
-  description = "value"
+  description = "name of the secret"
   sensitive   = false
 }
 
 output "kms_key_id" {
   value       = aws_secretsmanager_secret.this.kms_key_id
-  description = "value"
+  description = "id of the kms key used to encrypt the secret (if specified)"
   sensitive   = false
 }
 
 output "version_arn" {
   value       = aws_secretsmanager_secret_version.this.arn
-  description = "value"
+  description = "arn of the secret version"
   sensitive   = false
 }
 
 output "version_id" {
   value       = aws_secretsmanager_secret_version.this.id
-  description = "value"
+  description = "id of the secret version"
   sensitive   = false
 }
 
 output "version_secret_string" {
   value       = aws_secretsmanager_secret_version.this.secret_string
-  description = ""
+  description = "secret key-value pairs"
   sensitive   = true
 }
 
 output "version_version_id" {
   value       = aws_secretsmanager_secret_version.this.version_id
-  description = ""
+  description = "version id of the secret version"
   sensitive   = false
 }
