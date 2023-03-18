694624fd2539ae4450849dd241dedb1a876f01b4
Cole DeVries <colemandevries@gmail.com>
2023-03-18 16:12:00 -0400

    cleaned up task definition locals

--- terraform/aws/modules/ecs-task-definition/locals.tf
+++ terraform/aws/modules/ecs-task-definition/locals.tf
@@ -1,4 +1,3 @@
 locals {
   stack                    = "${var.program}-${var.env}-${var.app}"
-  permissions_boundary_arn = terraform.workspace == "dev" || terraform.workspace == "qa" ? "arn:aws:iam::${var.account_id}:policy/PermissionBoundary_PowerUser" : null
 }
