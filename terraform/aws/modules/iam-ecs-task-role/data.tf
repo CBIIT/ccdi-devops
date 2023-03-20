2c86ea2aa92940ed81fb59e0ff7ea6225a6b762d
Cole DeVries <colemandevries@gmail.com>
2023-03-20 19:54:52 -0400

    consolidating files into standard module structure

--- terraform/aws/modules/iam-ecs-task-role/data.tf
+++ terraform/aws/modules/iam-ecs-task-role/data.tf
@@ -1,1 +1,114 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:ecs:us-east-1:${var.account_id}:*"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [var.account_id]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "ecr" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "cloudwatch" {
+  count = var.attach_cloudwatch_policy ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams",
+      "logs:DescribeLogGroups"
+    ]
+    resources = [
+      "arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:*",
+      "arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:*:log-stream:*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "opensearch" {
+  count = var.attach_opensearch_policy ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "es:ESHttpDelete",
+      "es:ESHttpGet",
+      "es:ESHttpHead",
+      "es:ESHttpPatch",
+      "es:ESHttpPost",
+      "es:ESHttpPut"
+    ]
+    resources = ["${var.opensearch_domain_arn}/*"]
+  }
+}
+
+data "aws_iam_policy_document" "secrets_manager" {
+  count = var.attach_secrets_manager_policy ? 1 : 0
+
+  statement {
+    actions = [ 
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:ListSecretVersionIds",
+      "secretsmanager:ListSecrets",
+     ]
+  }
+}
+
+data "aws_iam_policy_document" "ecs_exec" {
+  count = var.attach_ecs_exec_policy ? 1 : 0
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ecs:ExecuteCommand"]
+    resources = [var.ecs_cluster_arn]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
+    ]
+    resources = [var.kms_key_arn]
+  }
+}
--- terraform/aws/modules/iam-ecs-task-role/main.tf
+++ terraform/aws/modules/iam-ecs-task-role/main.tf
@@ -5,3 +5,73 @@ resource "aws_iam_role" "this" {
   permissions_boundary = local.permissions_boundary_arn
 }
 
+resource "aws_iam_policy" "ecr" {
+  name        = "power-user-${local.stack}-task-policy-ecr-${var.microservice}"
+  description = "enables ecr access for the ${var.microservice} service in the ${var.env} tier"
+  policy      = data.aws_iam_policy_document.ecr[0].json
+}
+  
+resource "aws_iam_role_policy_attachment" "ecr" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.ecr.arn
+}
+
+resource "aws_iam_policy" "cloudwatch" {
+  count = var.attach_cloudwatch_policy ? 1 : 0
+
+  name        = "power-user-${local.stack}-task-policy-cloudwatch-${var.microservice}"
+  description = "enables cloudwatch logging for the ${var.microservice} service in the ${var.env} tier"
+  policy      = data.aws_iam_policy_document.cloudwatch[0].json
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  count = var.attach_cloudwatch_policy ? 1 : 0
+
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.cloudwatch[0].arn
+}
+
+resource "aws_iam_policy" "opensearch" {
+  count = var.attach_opensearch_policy ? 1 : 0
+
+  name        = "power-user-${local.stack}-task-policy-opensearch-${var.microservice}"
+  description = "enables opensearch access for the ${var.microservice} service in the ${var.env} tier"
+  policy      = data.aws_iam_policy_document.opensearch[0].json
+}
+
+resource "aws_iam_role_policy_attachment" "opensearch" {
+  count = var.attach_opensearch_policy ? 1 : 0
+
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.opensearch[0].arn
+}
+
+resource "aws_iam_policy" "secrets_manager" {
+  count = var.attach_secrets_manager_policy ? 1 : 0
+
+  name        = "power-user-${local.stack}-task-policy-secrets-manager-${var.microservice}"
+  description = "enables secrets manager access for the ${var.microservice} service in the ${var.env} tier"
+  policy      = data.aws_iam_policy_document.secrets_manager[0].json
+}
+
+resource "aws_iam_role_policy_attachment" "secrets_manager" {
+  count = var.attach_secrets_manager_policy ? 1 : 0
+
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.secrets_manager[0].arn
+}
+
+resource "aws_iam_policy" "ecs_exec" {
+  count = var.attach_ecs_exec_policy ? 1 : 0
+
+  name        = "power-user-${local.stack}-task-policy-ecs-exec-${var.microservice}"
+  description = "enables ecs execute commands for the ${var.microservice} service in the ${var.env} tier"
+  policy      = data.aws_iam_policy_document.ecs_exec[0].json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec" {
+  count = var.attach_ecs_exec_policy ? 1 : 0
+
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.ecs_exec[0].arn
+}
