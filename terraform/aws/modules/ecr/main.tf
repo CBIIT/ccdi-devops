4bb46478d9076fcfc6063a62bae288b4df3f085f
Kotepalli <kotepalliv2@nih.gov>
2023-03-23 19:37:03 -0400

    updated terraform/aws/modules/ecr/main.tf

--- terraform/aws/modules/ecr/main.tf
+++ terraform/aws/modules/ecr/main.tf
@@ -25,7 +25,7 @@ resource "aws_ecr_repository_policy" "this" {
 resource "aws_ecr_lifecycle_policy" "this" {
   count = var.create_lifecycle_policy ? 1 : 0
 
-  repository = aws_ecr_repository.this[0].name
+  repository = aws_ecr_repository.this.name
   policy     = <<EOF
 {
     "rules": [
