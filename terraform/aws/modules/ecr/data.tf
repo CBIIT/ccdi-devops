0eb6ae867462383f36a15c510b0e89fdf5f91e19
Kotepalli <kotepalliv2@nih.gov>
2023-03-23 16:57:48 -0400

    updated typo in principals

--- terraform/aws/modules/ecr/data.tf
+++ terraform/aws/modules/ecr/data.tf
@@ -3,7 +3,7 @@ data "aws_iam_policy_document" "this" {
 
   statement {
     effect    = "Allow"
-    principal = var.repository_policy_principal
+    principals = var.repository_policy_principal
     actions = [
       "ecr:GetDownloadUrlForLayer",
       "ecr:BatchGetImage",
