fccdea1a8af60ec602f2b048108784f19304a2c9
Cole DeVries <colemandevries@gmail.com>
2023-03-27 11:06:26 -0400

    updated new relic role module

---
+++ terraform/aws/modules/iam/new-relic/variables.tf
@@ -0,0 +1,33 @@
+variable "app" {
+  type        = string
+  description = "the name of the application expressed as an acronym"
+  sensitive   = false
+}
+
+variable "env" {
+  type        = string
+  description = "the target tier ('dev', 'qa', 'stage', or 'prod'.)"
+  sensitive   = false
+
+  validation {
+    condition     = contains(["dev", "qa", "stage", "prod", "nonprod"], var.env)
+    error_message = "valid values are 'dev', 'qa', 'stage', 'prod', and 'nonprod'"
+  }
+}
+
+variable "program" {
+  type        = string
+  description = "the program associated with the application"
+  sensitive   = false
+
+  validation {
+    condition     = contains(["crdc", "ccdi", "ctos"], var.program)
+    error_message = "valid values for program are 'crdc', 'ccdi', and 'ctos'"
+  }
+}
+
+variable "new_relic_external_id" {
+  type        = string
+  description = "the external id for the new relic account - seven digit number retrieved from the new relic account"
+  sensitive   = true
+}
