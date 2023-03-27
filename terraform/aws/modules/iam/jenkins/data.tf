ba558acb6c0fdf2a429ef5fc1aca6febfc89ef7b
Cole DeVries <colemandevries@gmail.com>
2023-03-27 10:39:13 -0400

    consolidating files down to standard module structure

--- terraform/aws/modules/iam/jenkins/data.tf
+++ terraform/aws/modules/iam/jenkins/data.tf
@@ -14,6 +14,220 @@ data "aws_iam_policy_document" "trust" {
 
 }
 
-data "aws_iam_policy_document" "iam" {
+data "aws_iam_policy_document" "ecr" {
+  count = var.enable_ecr_access ? 1 : 0
 
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:DescribeRegistry",
+      "ecr:GetAuthorizationToken",
+      "ecr:GetRegistryPolicy"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:InitiateLayerUpload",
+      "ecr:ListImages",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart"
+    ]
+    resources = var.ecr_repository_arns
+  }
+}
+
+data "aws_iam_policy_document" "ecs" {
+  count = var.enable_ecs_access ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:DeregisterContainerInstance",
+      "ecs:DescribeClusters",
+      "ecs:ListAttributes",
+      "ecs:ListContainerInstances",
+      "ecs:ListTagsForResource",
+      "ecs:RegisterContainerInstance",
+      "ecs:SubmitAttachmentStateChanges",
+      "ecs:SubmitContainerStateChange",
+      "ecs:SubmitTaskStateChange",
+      "ecs:TagResource",
+      "ecs:UntagResource",
+      "ecs:UpdateCluster",
+      "ecs:UpdateClusterSettings",
+    ]
+    resources = [
+      "arn:aws:ecs:us-east-1:${data.aws_caller_identity.current.account_id}:cluster/*"
+    ]
+
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:DeleteAttributes",
+      "ecs:DescribeContainerInstances",
+      "ecs:ListTagsForResource",
+      "ecs:ListTasks",
+      "ecs:Poll",
+      "ecs:PutAttributes",
+      "ecs:StartTelemetrySession",
+      "ecs:TagResource",
+      "ecs:UntagResource",
+      "ecs:UpdateContainerAgent",
+      "ecs:updateContainerInstancesState"
+    ]
+
+    resources = [
+      "arn:aws:ecs:us-east-1:${data.aws_caller_identity.current.account_id}:container-instance/*/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:ListTagsForResource",
+      "ecs:RunTask",
+      "ecs:StartTask",
+      "ecs:TagResource",
+      "ecs:UntagResource"
+    ]
+    resources = [
+      "arn:aws:ecs:us-east-1:${data.aws_caller_identity.current.account_id}:task-definition/*:*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:CreateService",
+      "ecs:DeleteService",
+      "ecs:DescribeServices",
+      "ecs:TagResource",
+      "ecs:UntagResource",
+      "ecs:UpdateService",
+    ]
+    resources = ["arn:aws:ecs:us-east-1:${data.aws_caller_identity.current.account_id}:service/*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:DescribeTasks",
+      "ecs:StopTask",
+      "ecs:ExecuteCommand",
+      "ecs:ListTagsForResource",
+      "ecs:TagResource",
+      "ecs:UntagResource",
+    ]
+    resources = [
+      "arn:aws:ecs:us-east-1:${data.aws_caller_identity.current.account_id}:task/*/*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:ListClusters",
+      "ecs:ListServices",
+      "ecs:DescribeTaskDefinition",
+      "ecs:DeregisterTaskDefinition",
+      "ecs:DiscoverPollEndpoint",
+      "ecs:ListClusters",
+      "ecs:ListServices",
+      "ecs:ListTaskDefinitionFamilies",
+      "ecs:ListTaskDefinitions",
+      "ecs:ListTasks",
+      "ecs:RegisterTaskDefinition"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "iam" {
+  count = terraform.workspace == "dev" || terraform.workspace == "stage" ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:GetRole"
+    ]
+    resources = [
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:PassRole"
+    ]
+    resources = [
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "opensearch" {
+  count = var.enable_opensearch_access ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "es:ESHttpDelete",
+      "es:ESHttpGet",
+      "es:ESHttpHead",
+      "es:ESHttpPatch",
+      "es:ESHttpPost",
+      "es:ESHttpPut"
+    ]
+    resources = var.opensearch_domain_arns
+  }
+}
+
+data "aws_iam_policy_document" "s3" {
+  count = var.enable_s3_access ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAttributes",
+      "s3:GetObjectVersion",
+      "s3:ListAllMyBuckets",
+      "s3:ListBucket",
+      "s3:ListBucketVersions",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+    resources = var.s3_bucket_arns
+  }
+}
+
+data "aws_iam_policy_document" "secrets" {
+  count = var.enable_secrets_manager_access ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:CreateSecret",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetRandomPassword",
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:ListSecretVersionIds",
+      "secretsmanager:ListSecrets",
+      "secretsmanager:PutSecretValue",
+      "secretsmanager:RestoreSecret"
+    ]
+    resources = var.secrets_manager_secret_arns
+  }
 }
--- terraform/aws/modules/iam/jenkins/main.tf
+++ terraform/aws/modules/iam/jenkins/main.tf
@@ -6,3 +6,95 @@ resource "aws_iam_role" "this" {
   name = "power-user-${local.stack}-jenkins-instance-profile-role"
 }
 
+resource "aws_iam_policy" "ecr" {
+  count = var.enable_ecr_access ? 1 : 0
+
+  name        = "power-user-${local.stack}-jenkins-instance-profile-role-ecr"
+  description = "allow jenkins to pull and push from specified ecr repositories"
+  policy      = data.aws_iam_policy_document[0].ecr
+  tags        = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ecr" {
+  count = var.enable_ecr_access ? 1 : 0
+
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.ecr[0].arn
+}
+
+resource "aws_iam_policy" "ecs" {
+  count = var.enable_ecs_access ? 1 : 0
+
+  name        = "power-user-${local.stack}-jenkins-instance-profile-role-ecs"
+  description = "allow jenkins to access specified ecs clusters to perform deployment operations"
+  policy      = data.aws_iam_policy_document.ecs[0].json
+  tags        = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ecs" {
+  count = var.enable_ecs_access ? 1 : 0
+
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.ecs[0].arn
+}
+
+resource "aws_iam_policy" "iam" {
+  name        = "power-user-${local.stack}-jenkins-instance-profile-role-iam"
+  description = "allow jenkins to get and pass iam roles required for deployment operations"
+  policy      = data.aws_iam_policy_document.iam.json
+  tags        = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "iam" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.iam.arn
+}
+
+
+resource "aws_iam_policy" "opensearch" {
+  count = var.enable_opensearch_access ? 1 : 0
+
+  name        = "power-user-${local.stack}-jenkins-instance-profile-role-opensearch"
+  description = "allow jenkins to perform etl activities on specified opensearch clusters"
+  policy      = data.aws_iam_policy_document.opensearch[0].json
+  tags        = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "opensearch" {
+  count = var.enable_opensearch_access ? 1 : 0
+
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.opensearch[0].arn
+}
+
+resource "aws_iam_policy" "s3" {
+  count = var.enable_s3_access ? 1 : 0
+
+  name        = "power-user-${local.stack}-jenkins-instance-profile-role-s3"
+  description = "allow jenkins to read from specified s3 buckets"
+  policy      = data.aws_iam_policy_document.s3[0].json
+  tags        = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "s3" {
+  count = var.enable_s3_access ? 1 : 0
+
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.s3[0].arn
+}
+
+resource "aws_iam_policy" "secrets" {
+  count = var.enable_secrets_manager_access ? 1 : 0
+
+  name        = "power-user-${local.stack}-jenkins-instance-profile-role-secrets"
+  description = "allow jenkins to read from specified secrets manager secrets"
+  policy      = data.aws_iam_policy_document.secrets[0].json
+  tags        = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "secrets" {
+  count = var.enable_secrets_manager_access ? 1 : 0
+
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.secrets[0].arn
+}
