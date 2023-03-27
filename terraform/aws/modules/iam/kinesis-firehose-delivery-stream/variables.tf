26939f43d425d3734585bfcd46f4e7e6929eb27f
colemandevries <colemandevries@gmail.com>
2023-03-27 15:08:45 -0400

    created kinesis-firehose-delivery-stream iam module

---
+++ terraform/aws/modules/iam/kinesis-firehose-delivery-stream/variables.tf
@@ -0,0 +1,39 @@
+variable "app" {
+  type        = string
+  description = "the name of the application expressed as an acronym"
+  sensitive   = false
+}
+
+variable "env" {
+  type        = string
+  description = "the target tier ('dev', 'qa', 'stage', or 'prod'.)"
+  sensitive   = false
+
+  validation {
+    condition     = contains(["dev", "qa", "stage", "prod", "nonprod"], var.env)
+    error_message = "valid values are 'dev', 'qa', 'stage', 'prod', and 'nonprod'"
+  }
+}
+
+variable "program" {
+  type        = string
+  description = "the program associated with the application"
+  sensitive   = false
+
+  validation {
+    condition     = contains(["crdc", "ccdi", "ctos"], var.program)
+    error_message = "valid values for program are 'crdc', 'ccdi', and 'ctos'"
+  }
+}
+
+variable "attach_permissions_boundary" {
+  type        = bool
+  description = "whether to attach permissions boundary to the role"
+  sensitive   = false
+}
+
+variable "s3_bucket_arn" {
+  type        = string
+  description = "the arn of the s3 bucket to deliver failed messages to"
+  sensitive   = false
+}
