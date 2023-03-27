26a99b5c6652e4dfc31c4148ee23a624f85d68c0
colemandevries <colemandevries@gmail.com>
2023-03-27 12:03:19 -0400

    fixed permissions boundary locals issue with new relic module

--- terraform/aws/modules/iam/new-relic/main.tf
+++ terraform/aws/modules/iam/new-relic/main.tf
@@ -3,7 +3,7 @@ resource "aws_iam_role" "new_relic_read_only" {
   assume_role_policy    = data.aws_iam_policy_document.trust.json
   description           = "allow new relic to retrieve observability data from cloudwatch"
   force_detach_policies = false
-  permissions_boundary  = local.permissions_boundary
+  permissions_boundary  = local.permissions_boundary_arn
 }
 
 resource "aws_iam_role_policy_attachment" "new_relic_read_only" {
