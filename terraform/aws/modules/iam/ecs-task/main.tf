c9fd6aee3ce62f65b42c3d86033585d68b1dba32
Cole DeVries <colemandevries@gmail.com>
2023-03-27 20:02:18 -0400

    fixed the ecs-task iam role module microservice constraint

--- terraform/aws/modules/iam/ecs-task/main.tf
+++ terraform/aws/modules/iam/ecs-task/main.tf
@@ -1,13 +1,13 @@
 resource "aws_iam_role" "this" {
   name                 = "power-user-${local.stack}-ecs-task-role"
-  description          = "ecs task role for ${var.microservice} in the ${var.env} tier"
+  description          = "ecs task role for the ${local.stack} services"
   assume_role_policy   = data.aws_iam_policy_document.trust.json
   permissions_boundary = local.permissions_boundary_arn
 }
 
 resource "aws_iam_policy" "ecr" {
-  name        = "power-user-${local.stack}-task-policy-ecr-${var.microservice}"
-  description = "enables ecr access for the ${var.microservice} service in the ${var.env} tier"
+  name        = "power-user-${local.stack}-task-policy-ecr"
+  description = "enables ecr access for the ${local.stack} services"
   policy      = data.aws_iam_policy_document.ecr.json
 }
 
@@ -19,8 +19,8 @@ resource "aws_iam_role_policy_attachment" "ecr" {
 resource "aws_iam_policy" "cloudwatch" {
   count = var.attach_cloudwatch_policy ? 1 : 0
 
-  name        = "power-user-${local.stack}-task-policy-cloudwatch-${var.microservice}"
-  description = "enables cloudwatch logging for the ${var.microservice} service in the ${var.env} tier"
+  name        = "power-user-${local.stack}-task-policy-cloudwatch"
+  description = "enables cloudwatch logging for the ${local.stack} services"
   policy      = data.aws_iam_policy_document.cloudwatch[0].json
 }
 
@@ -34,8 +34,8 @@ resource "aws_iam_role_policy_attachment" "cloudwatch" {
 resource "aws_iam_policy" "opensearch" {
   count = var.attach_opensearch_policy ? 1 : 0
 
-  name        = "power-user-${local.stack}-task-policy-opensearch-${var.microservice}"
-  description = "enables opensearch access for the ${var.microservice} service in the ${var.env} tier"
+  name        = "power-user-${local.stack}-task-policy-opensearch"
+  description = "enables opensearch access for the ${local.stack} services"
   policy      = data.aws_iam_policy_document.opensearch[0].json
 }
 
@@ -49,8 +49,8 @@ resource "aws_iam_role_policy_attachment" "opensearch" {
 resource "aws_iam_policy" "secrets_manager" {
   count = var.attach_secrets_manager_policy ? 1 : 0
 
-  name        = "power-user-${local.stack}-task-policy-secrets-manager-${var.microservice}"
-  description = "enables secrets manager access for the ${var.microservice} service in the ${var.env} tier"
+  name        = "power-user-${local.stack}-task-policy-secrets-manager"
+  description = "enables secrets manager access for the ${local.stack} services"
   policy      = data.aws_iam_policy_document.secrets_manager[0].json
 }
 
@@ -64,8 +64,8 @@ resource "aws_iam_role_policy_attachment" "secrets_manager" {
 resource "aws_iam_policy" "ecs_exec" {
   count = var.attach_ecs_exec_policy ? 1 : 0
 
-  name        = "power-user-${local.stack}-task-policy-ecs-exec-${var.microservice}"
-  description = "enables ecs execute commands for the ${var.microservice} service in the ${var.env} tier"
+  name        = "power-user-${local.stack}-task-policy-ecs-exec"
+  description = "enables ecs execute commands for the ${local.stack} services"
   policy      = data.aws_iam_policy_document.ecs_exec[0].json
 }
 
