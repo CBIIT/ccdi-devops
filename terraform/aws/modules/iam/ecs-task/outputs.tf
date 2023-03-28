494bd0ae3676d44ad793a7130c486ca49f7bc457
Cole DeVries <colemandevries@gmail.com>
2023-03-27 20:19:50 -0400

    fixed iam ecs resource outputs for modules

--- terraform/aws/modules/iam/ecs-task/outputs.tf
+++ terraform/aws/modules/iam/ecs-task/outputs.tf
@@ -1,16 +1,16 @@
-output "task_role_arn" {
+output "arn" {
   value       = aws_iam_role.this.arn
   description = "arn of the task role if one was created"
   sensitive   = false
 }
 
-output "task_role_id" {
+output "id" {
   value       = aws_iam_role.this.id
   description = "id of the task role if one was created"
   sensitive   = false
 }
 
-output "task_role_name" {
+output "name" {
   value       = aws_iam_role.this.name
   description = "name of the task role if one was created"
   sensitive   = false
