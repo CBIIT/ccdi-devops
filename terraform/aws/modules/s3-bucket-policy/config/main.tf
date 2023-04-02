f85f45bf2bd2a4b1c0e9f2ef745fd23d859e3ec7
Cole DeVries <colemandevries@gmail.com>
2023-04-02 19:35:02 -0400

    updated github actions, updated config rules, created config-recorder, created s3-bucket-policies

---
+++ terraform/aws/modules/s3-bucket-policy/config/main.tf
@@ -0,0 +1,50 @@
+data "aws_organizations_organization" "current" {}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+
+
+
+    actions = [
+      "s3:GetBucketAcl",
+      "s3:ListBucket",
+    ]
+    resources = [var.s3_bucket_arn]
+
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalOrgID"
+      values   = [data.aws_organizations_organization.current.id]
+    }
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions   = ["s3:PutObject"]
+    resources = ["${var.s3_bucket_arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalOrgID"
+      values   = [data.aws_organizations_organization.current.id]
+    }
+  }
+}
