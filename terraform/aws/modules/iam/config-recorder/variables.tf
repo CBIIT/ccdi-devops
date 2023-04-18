af4bf2abb13cc7ec844a891af61c22932cc19b8e
colemandevries <colemandevries@gmail.com>
2023-04-18 14:50:20 -0400

    config recorder module iam role

---
+++ terraform/aws/modules/iam/config-recorder/variables.tf
@@ -0,0 +1,34 @@
+variable "app" {
+  type        = string
+  description = "the name of the application expressed as an acronym"
+  sensitive   = false
+}
+
+variable "env" {
+  type        = string
+  description = "the target tier ('dev', 'qa', 'stage', or 'prod'.)"
+  sensitive   = false
+
+  validation {
+    condition     = contains(["dev", "qa", "stage", "prod", "nonprod"], var.env)
+    error_message = "valid values are 'dev', 'qa', 'stage', 'prod', and 'nonprod'"
+  }
+}
+
+variable "program" {
+  type        = string
+  description = "the program associated with the application"
+  sensitive   = false
+
+  validation {
+    condition     = contains(["crdc", "ccdi", "ctos"], var.program)
+    error_message = "valid values for program are 'crdc', 'ccdi', and 'ctos'"
+  }
+}
+
+variable "attach_permissions_boundary" {
+  type        = bool
+  description = "whether or not to attach a permissions boundary to the role"
+  default     = false
+  sensitive   = false
+}
