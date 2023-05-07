554fe6181846164aef086d7cc1d4891bebee4f61
Cole DeVries <colemandevries@gmail.com>
2023-05-07 16:57:40 -0400

    updating the alb construct

--- terraform/constructs/alb/main.tf
+++ terraform/constructs/alb/main.tf
@@ -2,82 +2,11 @@ module "alb" {
   source = "../../modules/alb/"
 
   access_logs_bucket          = var.access_logs_bucket
-  access_logs_enabled         = var.access_logs_enabled
-  alb_listener_ssl_policy     = var.alb_listener_ssl_policy
   app                         = var.app
-  domain_certificate_arn      = var.domain_certificate_arn
-  drop_invalid_header_fields  = var.drop_invalid_header_fields
   enable_deletion_protection  = var.enable_deletion_protection
-  fixed_response_content_type = var.fixed_response_content_type
-  fixed_response_message_body = var.fixed_response_message_body
-  fixed_response_status_code  = var.fixed_response_status_code
-  http_port                   = var.http_port
-  http_protocol               = var.http_protocol
-  http_redirect_status_code   = var.http_redirect_status_code
-  https_port                  = var.https_port
-  https_protocol              = var.https_protocol
+  env                         = var.env
   internal                    = var.internal
-  load_balancer_type          = var.load_balancer_type
   program                     = var.program
   security_group_id           = var.security_group_id
-  timeout_create              = var.timeout_create
-  public_subnets              = var.public_subnets
-  tier                        = var.tier
+  subnets                     = var.subnets
 }
-
-# module "security_group" {
-#   source = "../../modules/security-groups/alb-sg/"
-
-#   app                         = var.app
-#   program                     = var.program
-#   security_group_ingress_cidr = var.security_group_ingress_cidr
-#   tier                        = var.tier
-#   vpc_id                      = var.vpc_id
-#   //  security_group_nih_cidrs    = var.security_group_nih_cidrs
-# }
-
-# module "frontend_target_group" {
-#   source = "../../modules/target-group/"
-
-#   app                              = var.app
-#   health_check_path                = var.frontend_health_check_path
-#   health_check_protocol            = var.protocol
-#   health_check_port                = var.health_check_port
-#   health_check_matcher             = var.health_check_matcher
-#   health_check_interval            = var.health_check_interval
-#   health_check_timeout             = var.health_check_timeout
-#   health_check_healthy_threshold   = var.health_check_healthy_threshold
-#   health_check_unhealthy_threshold = var.health_check_unhealthy_threshold
-#   protocol                         = var.protocol
-#   stickiness_cookie_duration       = var.stickiness_cookie_duration
-#   stickiness_enabled               = var.stickiness_enabled
-#   stickiness_type                  = var.stickiness_type
-#   port                             = var.frontend_port
-#   program                          = var.program
-#   tier                             = var.tier
-#   target_description               = var.frontend_target_description
-#   vpc_id                           = var.vpc_id
-# }
-
-# module "backend_target_group" {
-#   source = "../../modules/target-group/"
-
-#   app                              = var.app
-#   health_check_path                = var.backend_health_check_path
-#   health_check_protocol            = var.health_check_protocol
-#   health_check_port                = var.health_check_port
-#   health_check_matcher             = var.health_check_matcher
-#   health_check_interval            = var.health_check_interval
-#   health_check_timeout             = var.health_check_timeout
-#   health_check_healthy_threshold   = var.health_check_healthy_threshold
-#   health_check_unhealthy_threshold = var.health_check_unhealthy_threshold
-#   protocol                         = var.protocol
-#   stickiness_cookie_duration       = var.stickiness_cookie_duration
-#   stickiness_enabled               = var.stickiness_enabled
-#   stickiness_type                  = var.stickiness_type
-#   port                             = var.backend_port
-#   program                          = var.program
-#   tier                             = var.tier
-#   target_description               = var.backend_target_description
-#   vpc_id                           = var.vpc_id
-# }
--- terraform/constructs/alb/variables.tf
+++ terraform/constructs/alb/variables.tf
@@ -1,10 +1,6 @@
-##################################
-#  Required Variables ############
-##################################
-
-variable "security_group_id" {
-  type = list(string) 
-  
+variable "app" {
+  type        = string
+  description = "The name of the application that the load balancer belongs to"
 }
 
 variable "access_logs_bucket" {
@@ -12,19 +8,8 @@ variable "access_logs_bucket" {
   description = "The log bucket to send S3 access logs to"
 }
 
-variable "access_logs_enabled" {
-  type    = bool
-  default = true
-}
-
-variable "app" {
-  type        = string
-  description = "The name of the application that the load balancer belongs to"
-}
-
-variable "domain_certificate_arn" {
-  type        = string
-  description = "The ARN of the certificate manager domain certificate for the ALB"
+variable "env" {
+  type = string
 }
 
 variable "internal" {
@@ -37,205 +22,11 @@ variable "program" {
   description = "The name of the program this app or project supports (i.e. ccdi)"
 }
 
-variable "public_subnets" {
+variable "security_group_id" {
+  type = list(string)  
+}
+
+variable "subnets" {
   type        = set(string)
   description = "The subnets associated with the load balancer"
 }
-
-variable "security_group_ingress_cidr" {
-  type = list(string)
-}
-
-//variable "security_group_nih_cidrs" {
-//  type = list(string)
-//}
-
-variable "tier" {
-  type        = string
-  description = "The tier or environment that the load balancer belongs to"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID for the target group"
-}
-
-##################################
-#  Optional Variables ############
-##################################
-
-variable "alb_listener_ssl_policy" {
-  type        = string
-  description = "The ssl policy to associate with the HTTPS listener for the ALB"
-  default     = "ELBSecurityPolicy-2016-08"
-}
-
-variable "backend_port" {
-  type    = string
-  default = "8080"
-}
-
-variable "backend_target_description" {
-  type        = string
-  description = "Describing the target, such as frontend or backend"
-  default     = "backend"
-}
-
-variable "drop_invalid_header_fields" {
-  type        = bool
-  description = "Indecates whether or not headers with header fields that are not valid are removed by the load balancer (true) or routed to targets (false)"
-  default     = true
-}
-
-variable "enable_deletion_protection" {
-  type        = bool
-  description = "If true, deletion of the load balancer will be disabled via the AWS API. This will prevent Terraform from deleting the load balancer. Defaults to false."
-  default     = true
-}
-
-variable "fixed_response_content_type" {
-  type        = string
-  description = "The fixed response HTTPS content type"
-  default     = "text/plain"
-}
-
-variable "fixed_response_message_body" {
-  type        = string
-  description = "The default message body when an ALB is not allowing traffic from a fixed response action"
-  default     = "The application is not available at this time. Please try again soon."
-}
-
-variable "fixed_response_status_code" {
-  type        = string
-  description = "The fixed response status code"
-  default     = "200"
-}
-
-variable "frontend_port" {
-  type        = string
-  description = "The port for the frontend container"
-  default     = "80"
-}
-
-variable "frontend_target_description" {
-  type        = string
-  description = "Describing the target, such as frontend or backend"
-  default     = "frontend"
-}
-
-variable "health_check_interval" {
-  type    = number
-  default = 100
-}
-
-variable "health_check_matcher" {
-  type    = string
-  default = "200"
-}
-
-variable "frontend_health_check_path" {
-  type    = string
-  default = "/"
-}
-
-variable "backend_health_check_path" {
-  type    = string
-  default = "/ping"
-}
-
-variable "health_check_port" {
-  type    = string
-  default = "traffic-port"
-}
-
-variable "health_check_protocol" {
-  type    = string
-  default = "HTTP"
-}
-
-variable "health_check_timeout" {
-  type    = number
-  default = 60
-}
-
-variable "health_check_healthy_threshold" {
-  type    = number
-  default = 10
-}
-
-variable "http_port" {
-  type        = number
-  description = "The standard HTTP port"
-  default     = 80
-}
-
-variable "http_protocol" {
-  type        = string
-  description = "The standard HTTP protocol"
-  default     = "HTTP"
-}
-
-variable "http_redirect_status_code" {
-  type        = string
-  description = "The HTTP redirect to HTTPS default status code"
-  default     = "HTTP_301"
-}
-
-variable "https_port" {
-  type        = number
-  description = "The standard HTTPS port"
-  default     = 443
-}
-
-variable "https_protocol" {
-  type        = string
-  description = "The standard HTTPS protocol"
-  default     = "HTTPS"
-}
-
-variable "load_balancer_type" {
-  type        = string
-  description = "The type of load balancer to create. Possible values are application, gateway, or network. The default value is application"
-  default     = "application"
-}
-
-variable "protocol" {
-  type    = string
-  default = "HTTP"
-}
-
-variable "stickiness_cookie_duration" {
-  type        = number
-  description = "The time period, in seconds, during which requests from a client should be routed to the same target."
-  default     = 1800
-}
-
-variable "stickiness_enabled" {
-  type        = bool
-  description = "Enable or disable stickiness"
-  default     = true
-}
-
-variable "stickiness_type" {
-  type        = string
-  description = "The type of sticky sessions. The only current possible values are lb_cookie, app_cookie for ALBs, and source_ip for NLBs"
-  default     = "lb_cookie"
-}
-
-variable "health_check_unhealthy_threshold" {
-  type    = number
-  default = 10
-}
-
-variable "target_type" {
-  type    = string
-  default = "ip"
-}
-
-variable "timeout_create" {
-  type        = string
-  description = "The time allowed for timeout when creating the ALB resource."
-  default     = "10m"
-}
-
-
