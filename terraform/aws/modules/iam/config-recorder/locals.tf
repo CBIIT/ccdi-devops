f005f98c23464a351386611c35c125b35769dff3
colemandevries <colemandevries@gmail.com>
2023-05-09 11:11:39 -0400

    updated the config role policies to include additional managed policy

--- terraform/aws/modules/iam/config-recorder/locals.tf
+++ terraform/aws/modules/iam/config-recorder/locals.tf
@@ -1,5 +1,4 @@
 locals {
   stack                    = "${var.program}-${var.env}-${var.app}"
   permissions_boundary_arn = var.attach_permissions_boundary ? "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/PermissionBoundary_PowerUser" : null
-
 }
