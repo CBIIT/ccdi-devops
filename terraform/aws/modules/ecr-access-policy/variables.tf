a44153d69a87b8361b05d4f9acd209b4fb420ef5
Kotepalli <kotepalliv2@nih.gov>
2023-05-18 16:16:51 -0400

    updated terraform/aws/modules/ecr-access-policy/variables.tf

--- terraform/aws/modules/ecr-access-policy/variables.tf
+++ terraform/aws/modules/ecr-access-policy/variables.tf
@@ -32,3 +32,14 @@ variable "repository_name" {
   description = "the name of the ecr repository to associate the repository resource policy with"
   sensitive   = false
 }
+
+variable "access_policy_type" {
+  type        = string
+  description = "access policy type is to define what kind of policy to apply either standard, alternate or none"
+  default     = "standard"
+
+  validation {
+    condition     = var.my_variable == "standard" || var.my_variable == "alternate" || var.my_variable == "none"
+    error_message = "Invalid value for access_policy_type. Allowed options are: standard, alternate, none."
+  }
+}
