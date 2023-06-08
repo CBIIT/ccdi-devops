98ab5d30ab23b64ac83d275e784e4c98417674e8
colemandevries <colemandevries@gmail.com>
2023-06-08 10:01:47 -0400

    formatting only

--- terraform/aws/modules/s3-access-point/variables.tf
+++ terraform/aws/modules/s3-access-point/variables.tf
@@ -1,11 +1,11 @@
 variable "access_point_suffix" {
   type        = string
   description = "suffix to append to the access point name to describe its use or allowed principals"
-  sensitive = false
+  sensitive   = false
 }
 
 variable "bucket_name" {
   type        = string
   description = "name of the bucket to associate with the access point"
-  sensitive = false
+  sensitive   = false
 }
