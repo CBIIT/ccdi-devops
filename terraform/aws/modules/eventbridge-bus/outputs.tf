16e9db912af5fa79c6e59b69912e05453b723745
colemandevries <colemandevries@gmail.com>
2023-06-08 10:01:08 -0400

    created a new eventbridge bus module

---
+++ terraform/aws/modules/eventbridge-bus/outputs.tf
@@ -0,0 +1,17 @@
+output "" {
+  value       = aws_cloudwatch_event_bus.this.arn
+  description = "arn of the custom event bus"
+  sensitive   = false
+}
+
+output "id" {
+  value       = aws_cloudwatch_event_bus.this.id
+  description = "id of the custom event bus"
+  sensitive   = false
+}
+
+output "name" {
+  value       = aws_cloudwatch_event_bus.this.name
+  description = "name of the custom event bus"
+  sensitive   = false
+}
