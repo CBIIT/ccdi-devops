3fbbcb86e2458ca94a45b682949e2fcfdf9c8aff
Cole DeVries <colemandevries@gmail.com>
2023-06-23 08:13:44 -0400

    finished s3 access point module

--- terraform/aws/modules/s3-access-point/variables.tf
+++ terraform/aws/modules/s3-access-point/variables.tf
@@ -4,8 +4,28 @@ variable "access_point_suffix" {
   sensitive   = false
 }
 
+variable "account_id" {
+  type        = string
+  description = "the account id of the account that owns the access point - defaults to account id of the terraform provider"
+  default = null
+  sensitive   = false
+}
+
+variable "bucket_account_id" {
+  type        = string
+  description = "the account id of the account that owns the bucket - defaults to account id of the terraform provider"
+  default = null
+  sensitive   = false
+}
+
 variable "bucket_name" {
   type        = string
   description = "name of the bucket to associate with the access point"
+  sensitive   = false
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "the vpc id from which to allow access to the bucket via the access point"
   sensitive   = false
 }
