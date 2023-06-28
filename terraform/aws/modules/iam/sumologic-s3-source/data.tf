83f6cd235a53a850101f579282f6349cffbfd82b
Cole DeVries <colemandevries@gmail.com>
2023-06-28 08:03:29 -0400

    finished data sources

--- terraform/aws/modules/iam/sumologic-s3-source/data.tf
+++ terraform/aws/modules/iam/sumologic-s3-source/data.tf
@@ -1,3 +1,7 @@
+data "aws_caller_identity" "current" {
+  
+}
+
 data "aws_iam_policy_document" "trust" {
   statement {
     effect  = "Allow"
@@ -19,7 +23,7 @@ data "aws_iam_policy_document" "trust" {
 data "aws_iam_policy_document" "this" {
    dynamic "statement" {
     for_each = var.s3_bucket_ids
-    
+
     content {
       effect = "Allow"
       actions = [
