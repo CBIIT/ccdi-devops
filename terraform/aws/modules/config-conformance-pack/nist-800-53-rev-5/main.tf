739153215e9abd30658b7acd02f33ee71e4e4940
Cole DeVries <colemandevries@gmail.com>
2023-07-08 11:33:29 -0400

    updated the config-conformance-pack module

--- terraform/aws/modules/config-conformance-pack/nist-800-53-rev-5/main.tf
+++ terraform/aws/modules/config-conformance-pack/nist-800-53-rev-5/main.tf
@@ -1,10 +1,22 @@
 resource "aws_config_conformance_pack" "this" {
   name            = "fnl-aws-config-conformance-pack-nist-800-53-rev-5"
-  template_s3_uri = "s3://${var.config_s3_bucket_name}/${aws_s3_object.this.key}"
+  template_s3_uri = var.create_config_s3_bucket ? "s3://${module.s3[0].bucket}/${aws_s3_object.this.key}}" : "s3://${var.config_s3_bucket_name}/${aws_s3_object.this.key}"
 }
 
 resource "aws_s3_object" "this" {
-  bucket = var.config_s3_bucket_name
+  bucket = var.create_config_s3_bucket ? module.s3[0].id : var.config_s3_bucket_name
   key    = "templates/nist-800-53-rev-5.yaml"
   source = "${path.module}/nist-800-53-rev-5.yaml"
+}
+
+module "s3" {
+  count  = var.create_config_s3_bucket ? 1 : 0
+  source = "git::https://github.com/CBIIT/ccdi-devops.git//terraform/aws/modules/s3?ref=master"
+
+  app                = var.app
+  bucket_suffix      = "config-conformance-pack"
+  env                = var.env
+  force_destroy      = true
+  program            = var.program
+  versioning_enabled = true
 }
