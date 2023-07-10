37f4a17b444790305ec36e9680569ba60fdb1bf2
Cole DeVries <colemandevries@gmail.com>
2023-07-10 19:32:50 -0400

    major changes to the task definition module

--- terraform/aws/modules/ecs-task-definition/main.tf
+++ terraform/aws/modules/ecs-task-definition/main.tf
@@ -1,15 +1,20 @@
 resource "aws_ecs_task_definition" "this" {
-  family                   = "${local.stack}-${var.microservice}"
-  requires_compatibilities = var.requires_compatibilities
-  container_definitions    = var.container_definitions
-  network_mode             = var.network_mode
-  cpu                      = var.cpu
-  memory                   = var.memory
+  family                   = "${var.program}-${var.app}-${var.microservice}"
+  cpu                      = local.cpu
+  memory                   = local.memory
   execution_role_arn       = var.execution_role_arn
   task_role_arn            = var.task_role_arn
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
 
   runtime_platform {
-    operating_system_family = var.operating_system_family
-    cpu_architecture        = var.cpu_architecture
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
   }
+
+  container_definitions = jsondecode([
+    local.application_container_definition,
+    local.sumologic_container_definition,
+    local.newrelic_container_definition
+  ])
 }
