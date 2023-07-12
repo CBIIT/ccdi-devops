bbad97cc53c380e5055c93115cb37bb2c6d6926d
Cole DeVries <colemandevries@gmail.com>
2023-07-12 12:58:08 -0400

    updated the variables for kinesis s3 policy module

--- terraform/aws/modules/s3-bucket-policy/kinesis/variables.tf
+++ terraform/aws/modules/s3-bucket-policy/kinesis/variables.tf
@@ -1,9 +1,3 @@
-variable "principal_account_ids" {
-  type        = list(string)
-  description = "ids of the accounts from which to allow access to the bucket"
-  sensitive   = false
-}
-
 variable "s3_bucket_arn" {
   type        = string
   description = "arn of the s3 bucket to attach the policy to"
