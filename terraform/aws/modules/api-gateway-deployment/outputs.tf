e31a01cd3dab7dab3121a019c49cde229035e184
Cole DeVries <colemandevries@gmail.com>
2023-07-24 10:38:29 -0400

    updated outputs for modules

--- terraform/aws/modules/api-gateway-deployment/outputs.tf
+++ terraform/aws/modules/api-gateway-deployment/outputs.tf
@@ -1,22 +1,22 @@
-output "id" {
+output "deployment_id" {
   value       = aws_api_gateway_deployment.this.id
   description = "id of the deployment"
   sensitive   = false
 }
 
-output "invoke_url" {
+output "deployment_invoke_url" {
   value       = aws_api_gateway_deployment.this.invoke_url
   description = "url to invoke the api pointing to the stage"
   sensitive   = false
 }
 
-output "execution_arn" {
+output "deployment_execution_arn" {
   value       = aws_api_gateway_deployment.this.execution_arn
   description = "execution arn to be used in lambda_permission's source_arn when allowing API Gateway to invoke a Lambda function"
   sensitive   = false
 }
 
-output "created_date" {
+output "deployment_created_date" {
   value       = aws_api_gateway_deployment.this.created_date
   description = "creation date of the deployment"
   sensitive   = false
--- terraform/aws/modules/api-gateway-stage/outputs.tf
+++ terraform/aws/modules/api-gateway-stage/outputs.tf
@@ -1,22 +1,22 @@
-output "arn" {
+output "stage_arn" {
   value       = aws_api_gateway_stage.this.arn
   description = "arn of the api gateway stage resource"
   sensitive   = false
 }
 
-output "id" {
+output "stage_id" {
   value       = aws_api_gateway_stage.this.id
   description = "id of the api gateway stage resource"
   sensitive   = false
 }
 
-output "invoke_url" {
+output "stage_invoke_url" {
   value       = aws_api_gateway_stage.this.invoke_url
   description = "url to invoke the api pointing to the stage"
   sensitive   = false
 }
 
-output "execution_arn" {
+output "stage_execution_arn" {
   value       = aws_api_gateway_stage.this.execution_arn
   description = "execution arn to be used in lambda_permission's source_arn when allowing api gateway to invoke a lambda function"
   sensitive   = false
@@ -28,7 +28,7 @@ output "stage_name" {
   sensitive   = false
 }
 
-output "web_acl_arn" {
+output "stage_web_acl_arn" {
   value       = aws_api_gateway_stage.this.web_acl_arn
   description = "arn of the web acl associated with the stage"
   sensitive   = false
