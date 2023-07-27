dfa399874b8fabc36318ffd597afb03842d7c836
Cole DeVries <colemandevries@gmail.com>
2023-07-27 11:27:29 -0400

    Improved Lambda Function module

--- terraform/aws/modules/lambda-function/data.tf
+++ terraform/aws/modules/lambda-function/data.tf
@@ -39,6 +39,12 @@ data "aws_iam_policy_document" "this" {
   }
 
   statement {
+    effect = "Allow" 
+    actions = [ "rds-data:*" ]
+    resources = [ "*" ]
+  }
+
+  statement {
     effect = "Allow"
     actions = [
       "ec2:DescribeNetworkInterfaces",
