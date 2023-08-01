3074ae043d409dbe859f78eb9dd94a0366245858
Cole DeVries <colemandevries@gmail.com>
2023-08-01 09:18:17 -0400

    fixed spelling error

--- terraform/aws/modules/signer-signing-profile/locals.tf
+++ terraform/aws/modules/signer-signing-profile/locals.tf
@@ -1,4 +1,4 @@
-local {
+locals {
   stack    = "${var.program}-${var.env}-${var.app}-${var.platform}"
   platform = var.platform == "lambda" ? "AWSLambda-SHA384-ECDSA" : "Notation-OCI-SHA384-ECDSA"
 }
