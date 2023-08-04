f7d167a295a854a6fefae5956c73bcdb206a60f3
Cole DeVries <colemandevries@gmail.com>
2023-08-04 14:34:06 -0400

    updating lambda lifecycle and api gateway iam role

--- terraform/aws/modules/iam/api-gateway/locals.tf
+++ terraform/aws/modules/iam/api-gateway/locals.tf
@@ -1,4 +1,5 @@
 locals {
+  level                    = terraform.workspace == "dev" || terraform.workspace == "qa" ? "nonprod" : "prod"
   stack                    = "${var.program}-${var.env}-${var.app}"
   permissions_boundary_arn = var.attach_permissions_boundary ? "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/PermissionBoundary_PowerUser" : null
 }
