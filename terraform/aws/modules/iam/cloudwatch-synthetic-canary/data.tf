f689029d4bf73791356939f1c4e866c3de8b7d5d
Cole DeVries <colemandevries@gmail.com>
2023-12-19 09:19:04 -0500

    Updating CloudWatch Synthetic Canary IAM Role module

--- terraform/aws/modules/iam/cloudwatch-synthetic-canary/data.tf
+++ terraform/aws/modules/iam/cloudwatch-synthetic-canary/data.tf
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "this" {
       "s3:PutObject",
       "s3:GetObject"
     ]
-    resources = [var.s3_bucket_arn]
+    resources = ["${var.s3_bucket_arn}/*"]
   }
 
   statement {
