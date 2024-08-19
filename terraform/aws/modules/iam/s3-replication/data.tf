be61e8bfec917af56209864bcf3cdbb3f614f56e
Cole DeVries <colemandevries@gmail.com>
2024-08-19 15:13:06 -0400

    creating modules to support s3 replication

---
+++ terraform/aws/modules/iam/s3-replication/data.tf
@@ -0,0 +1,43 @@
+data "aws_iam_policy_document" "trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetReplicationConfiguration"
+    ]
+    resources = [var.source_bucket_arn]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionTagging"
+    ]
+    resources = ["${var.source_bucket_arn}/*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ReplicateObject",
+      "s3:ReplicateDelete",
+      "s3:ReplicateTags",
+      "s3:ObjectOwnerOverrideToBucketOwner"
+    ]
+    resources = ["${var.destination_bucket_arn}/*"]
+  }
+}
---
+++ terraform/aws/modules/iam/s3-replication/variables.tf
@@ -0,0 +1,46 @@
+variable "app" {
+  type        = string
+  description = "the name of the application expressed as an acronym"
+  sensitive   = false
+}
+
+variable "env" {
+  type        = string
+  description = "the target tier ('dev', 'qa', 'stage', 'nonprod' or 'prod'.)"
+  sensitive   = false
+
+  validation {
+    condition     = contains(["dev", "qa", "stage", "prod", "nonprod", "sandbox"], var.env)
+    error_message = "valid values are 'dev', 'qa', 'stage', 'prod', 'nonprod', and 'sandbox'"
+  }
+}
+
+variable "program" {
+  type        = string
+  description = "the program associated with the application"
+  sensitive   = false
+
+  validation {
+    condition     = contains(["crdc", "ccdi", "ctos", "fnl"], var.program)
+    error_message = "valid values for program are 'crdc', 'ccdi', 'fnl' and 'ctos'"
+  }
+}
+
+variable "attach_permissions_boundary" {
+  type        = bool
+  description = "whether to attach a permissions boundary to the role"
+  default     = false
+  sensitive   = false
+}
+
+variable "destination_bucket_arn" {
+  type        = string
+  description = "the ARN of the destination bucket"
+  sensitive   = false
+}
+
+variable "source_bucket_arn" {
+  type        = string
+  description = "the ARN of the source bucket"
+  sensitive   = false
+}
