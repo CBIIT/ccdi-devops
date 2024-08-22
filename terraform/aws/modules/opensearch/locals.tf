0b09947b80f1041fe1051a673bb9969d6471d82a
Cole DeVries <colemandevries@gmail.com>
2024-08-22 10:25:00 -0400

    updated the opensearch module outputs

--- terraform/aws/modules/opensearch/locals.tf
+++ terraform/aws/modules/opensearch/locals.tf
@@ -1,4 +1,6 @@
 locals {
-  stack  = "${var.program}-${var.env}-${var.app}"
-  ranges = ["129.43.0.0/16", "137.187.0.0/16", "10.128.0.0/9", "165.112.0.0/16", "156.40.0.0/16", "10.208.0.0/21", "128.231.0.0/16", "130.14.0.0/16", "157.98.0.0/16", "10.133.0.0/16"]
+  stack              = "${var.program}-${var.env}-${var.app}"
+  ranges             = ["129.43.0.0/16", "137.187.0.0/16", "10.128.0.0/9", "165.112.0.0/16", "156.40.0.0/16", "10.208.0.0/21", "128.231.0.0/16", "130.14.0.0/16", "157.98.0.0/16", "10.133.0.0/16"]
+  security_group_id  = var.create_security_group ? aws_security_group.this[0].id : "n/a"
+  security_group_arn = var.create_security_group ? aws_security_group.this[0].arn : "n/a"
 }
