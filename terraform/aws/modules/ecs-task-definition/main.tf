4ddbd1630ccab275a9fa9d042c3555aec907a3b4
Cole DeVries <colemandevries@gmail.com>
2024-10-31 13:34:07 -0400

    formatting and updating apigw module access logs

--- terraform/aws/modules/ecs-task-definition/main.tf
+++ terraform/aws/modules/ecs-task-definition/main.tf
@@ -19,7 +19,7 @@ resource "aws_ecs_task_definition" "this" {
   ])
 
   lifecycle {
-    ignore_changes = [ container_definitions, family ]
+    ignore_changes = [container_definitions, family]
   }
 }
 
