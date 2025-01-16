1e86c7b7d4c79e814d1dee1c29ec85c4afc1313b
Michael Fleming <michael.fleming@nih.gov>
2025-01-15 20:14:16 -0500

    Update variables.tf
    Signed-off-by: Michael Fleming <michael.fleming@nih.gov>

--- terraform/aws/modules/ecr-lifecycle-policy/variables.tf
+++ terraform/aws/modules/ecr-lifecycle-policy/variables.tf
@@ -3,3 +3,17 @@ variable "repository_name" {
   description = "the name of the ecr repository to associate the lifecycle policy with"
   sensitive   = false
 }
+
+variable "dev_image_limit" {
+  type        = number
+  description = "the number of non-prod images to retain in the repo"
+  sensitive   = false
+  default = 30
+}
+
+variable "prod_image_limit" {
+  type        = number
+  description = "the number of prod images to retain in the repo"
+  sensitive   = false
+  default = 10
+}
